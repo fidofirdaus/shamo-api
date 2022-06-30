b90bdcdd00f0982ee156f8a01d1be7858f5a35fc
fido2001 <firdauspriyono17@gmail.com>
2022-06-30 17:40:31 +0700

    Upload Project

---
+++ database/migrations/2022_06_23_020256_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id');
+
+            $table->text('address')->nullable();
+            $table->string('payment', 20)->default('MANUAL');
+            $table->float('total_price');
+            $table->float('shipping_price');
+            $table->string('status', 30)->default('PENDING');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
